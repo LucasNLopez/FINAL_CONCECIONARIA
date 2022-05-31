e5d8e1b22894bc19b7da6bc69248857f8e19cab6
EvanReesh <lucaslopezlnlaba@gmail.com>
2022-05-31 19:44:15 -0300

    nice

--- cuentas.py
+++ cuentas.py
@@ -1,4 +1,5 @@
 from usuarios import *
+import os
 cuentas = []
 cuenta_invitado=Invitado("invitado@gmail.com","123")
 cuenta_administrador=Administrador("lucaslopez@gmail.com","admin123")
@@ -7,19 +8,25 @@ cuentas.append(cuenta_invitado)
 cuentas.append(cuenta_administrador)
 
 def validar(seleccion):
-    if seleccion == 1:
-        print("BIENVENIDO AL MENU ADMINISTRADOR")
-        print("Ingrese el e-mail y luego la contraseña")
-        mail = input ()
-        contraseña = input ()
-        for i in range(len(cuentas)):
-            if mail == "lucaslopez@gmail.com" and contraseña == "admin123":
-                return cuentas[i]
-    elif seleccion == 2:
-        print ("MENU INVITADO")
-        print ("Ingrese e-mail y contraseña")
-        mail = input ()
-        contraseña = input ()
-        for i in range(len(cuentas)):
-            if mail == "invitado@gmail.com" and contraseña == "123":
-                return cuentas[i]
+    lu=3
+    while lu != 0:
+        if seleccion == 1:
+            print("BIENVENIDO AL MENU ADMINISTRADOR")
+            print("Ingrese el e-mail y luego la contraseña")
+            mail = input ()
+            contraseña = input ()
+            for i in range(len(cuentas)):
+                if mail == "lucaslopez@gmail.com" and contraseña == "admin123":
+                    return cuentas[i]
+        elif seleccion == 2:
+            print ("MENU INVITADO")
+            print ("Ingrese e-mail y contraseña")
+            mail = input ()
+            contraseña = input ()
+            for i in range(len(cuentas)):
+                if mail == "invitado@gmail.com" and contraseña == "123":
+                    return cuentas[i]
+        elif seleccion == 3:
+            lu = 0
+            os.system("cls")
+            print("Hasta Luego.")
