470a19ce5657e76f820f40075a511858d64bd12a
EvanReesh <lucaslopezlnlaba@gmail.com>
2022-06-28 19:24:06 -0300

    asd2da

--- ventana.py
+++ ventana.py
@@ -1,14 +1,4 @@
-from distutils.command.config import config
-from email.mime import image
-from textwrap import fill
 from tkinter import *
-from turtle import bgcolor
-from objetos import *
-from cuentas import *
-from usuarios import *
-from edit import *
-from menu import *
-import os
 from objetos import *
 from cuentas import *
 from usuarios import *
@@ -19,6 +9,8 @@ from agregar import *
 ##cursor = hand2 ( cambio de cursor para ponerselo en un frame)
     
 
+
+
 root = Tk()
 root.title("Panel")
 root.iconbitmap("auto.ico")
@@ -26,7 +18,6 @@ root.geometry("800x550")
 root.config(bd=18)
 root.config(relief="groove")
 root.config(bg="#5458C2")
-#TITULO CONCESIONARIA VISUAL
 titleFrame=Frame(root)
 titleFrame.pack(fill="x")
 titleFrame.config(bg="#5458C2",bd=5,relief="ridge")
@@ -36,20 +27,40 @@ imagenlabel.pack(fill="x")
 title_font=("Courier",20,"bold")
 titlelabel=Label(titleFrame, text="CONCESIONARIA",font=title_font)
 titlelabel.config(bg="#5458C2")
-titlelabel.pack()
+titlelabel.pack()   
 
 #########LOGGIN DEL PROGRAMA
+
+
+#$def login():
+    #if Administrador(user=usuario,contra=contraseña):
+        #lset("Bienvenido al menu Administrador")
+    #else:
+        #False
+
+
 miFrame=Frame(root)
 miFrame.pack()
 miFrame.config(bg="#5458C2")
+
+
+
+
+
+
 #### Entrada de usuario
+usuario=StringVar()
+contraseña=StringVar()
 usuario=Entry(miFrame)
 usuario.grid(row=0,column=1,padx=5,pady=5)
 usuario.config(justify="center")
+
+
 #### Entrada de contraseña
-contraseña=Entry(miFrame,)
+contraseña=Entry(miFrame)
 contraseña.grid(row=1,column=1,padx=5,pady=5)
 contraseña.config(justify="center",show="*")
+
 
 texto_usuario=Label(miFrame,text="Usuario:",bg="#5458C2")
 texto_usuario.grid(row=0,column=0,sticky="e",padx=5,pady=5)
@@ -59,13 +70,7 @@ texto_contra=Label(miFrame,text="Contraseña:",bg="#5458C2")
 texto_contra.grid(row=1,column=0,sticky="w",padx=5,pady=5)
 
 #BOTON ENVIAR LOGGIN
-botonEnviar=Button(root, text="Enviar",bg="#5458C2",)
-botonEnviar.pack()
-
-lista_archivos = os.listdir()
-
-
-
+Button(root, text="Acceder",bg="#5458C2").pack()
 
 
 
@@ -73,3 +78,7 @@ lista_archivos = os.listdir()
 
 
 root.mainloop()
+
+
+
+
